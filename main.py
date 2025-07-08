03dd3f8afadcb314ec022d3b5d2107d0ced453b0
hayksakulyan <sakulyanhayk@gmail.com>
2025-07-08 11:07:05 +0400

    "Insterted some files"

---
+++ main.py
@@ -0,0 +1,12 @@
+import calendar 
+
+print('Hello from super calendar\n')
+
+year = int(input('Enter year: '))
+month = int(input('Enter month: '))
+
+
+print(calendar.month(year, month))
+
+
+print('Have good day!')
